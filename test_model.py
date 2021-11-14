4aed7703eb0cc76083c044d987110100bdaacfad
Sebastian Rassmann <s.rassmann@uni-bonn.de>
2021-11-14 19:33:19 +0100

    Refactor project structure
    Rename main to train_model and create test_model for testing

---
+++ test_model.py
@@ -0,0 +1,34 @@
+import logging, logging.config
+from lib.utils import LOG_CONFIG
+
+logging.config.dictConfig(LOG_CONFIG)
+
+import sys, pickle, os, yaml
+from argparse import ArgumentParser
+
+sys.path.append("..")
+
+from lib import utils, testing
+from lib.models import *
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--ckp_path", type=str)
+    parser.add_argument("--output_dir", type=str, default=None)
+    parser.add_argument("--model", type=str, default="dbam_efficientnet-b0")
+    parser.add_argument("--input_height", type=int, default=512)
+    parser.add_argument("--input_width", type=int, default=512)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--num_workers", type=int, default=4)
+    parser.add_argument(
+        "--data_dir", type=str, default="../data/annotated/rsna_bone_age"
+    )
+    parser.add_argument("--mask_dirs", nargs="+", default=None)
+    parser = testing.add_eval_args(parser)
+    args = parser.parse_args()
+
+    utils.log_system_info(logger)
+    testing.evaluate_bone_age_model(args.ckp_path, args, args.output_dir)
+
+if __name__ == "__main__":
+    main()
