5c7c854fb9fa74aa6d954c7582fe6fb9cb3dc5ef
Sebastian Rassmann <s.rassmann@uni-bonn.de>
2022-11-17 17:46:46 +0100

    Clean up reproducibility

--- bash/slurm_job_example.sh
+++ bash/slurm_job_example.sh
@@ -1,23 +1,23 @@
 #! /bin/bash
-#SBATCH --job-name="bone-age_multitask"
+#SBATCH --job-name="bone-age_sh"
 #SBATCH --partition="ampere"
 #SBATCH --mem="30G"
-#SBATCH --cpus-per-task="16"
-#SBATCH --gres gpu:1,localtmp:20G
-#SBATCH --time="12:00:00"
-#SBATCH --output=/ceph01/projects/bone2gene/bone-age/output/slurm/%j.out
+#SBATCH --cpus-per-task="32"
+#SBATCH --gres gpu:1,localtmp:25G
+#SBATCH --time="22:00:00"
+#SBATCH --output=/ceph01/projects/bone2gene/bone-age2/output/slurm/%j.out
+#SBATCH --signal=SIGUSR1@90
 
-NAME="multitask_effnet_fancy_aug"
+NAME="masked_effnet_shallow_fancy_aug"
 
 module load anaconda/4.6.11-py37
-conda activate ptl
-cd /ceph01/projects/bone2gene/bone-age/
+conda activate ptl16
+cd /ceph01/projects/bone2gene/bone-age2/
 DATA_DIR="../data"
 
 python ../parallel_copy.py -s ../data/masks/tensormask/bone_age -o $SCRATCH_DIR/data/masks/tensormask/bone_age -d 1
 python ../parallel_copy.py -s ../data/masks/unet/bone_age -o $SCRATCH_DIR/data/masks/unet/bone_age -d 1
-python ../parallel_copy.py -s ../data/annotated/rsna_bone_age -o $SCRATCH_DIR/data/annotated/rsna_bone_age -d 2
-python ../parallel_copy.py -s ../data/annotated/rsna_bone_age -o $SCRATCH_DIR/data/annotated/rsna_bone_age -d 1
+python ../parallel_copy.py -s ../data/annotated/bone_age -o $SCRATCH_DIR/data/annotated/bone_age -d 1
 DATA_DIR="$SCRATCH_DIR/data"
 
 out_root=output/$NAME
@@ -26,27 +26,9 @@ mkdir "$out_root"
 LOG_FILE="$out_root/version_0".log
 
 LOG_FILE=$LOG_FILE python train_model.py \
-  --name $NAME \
-  --gpus 1 \
-  --precision 16\
-  --data_dir "$DATA_DIR/annotated/rsna_bone_age"\
-  --mask_dirs "$DATA_DIR/masks/tensormask/bone_age" "$DATA_DIR/masks/unet/bone_age" \
-  --max_time 11:30:00:00 \
-  --input_height 512\
-  --input_width 512\
-  --num_workers 16\
-  --model dbam_efficientnet-b0 \
-  --max_epoch 300\
-  --batch_size 32\
-  --learning_rate 1e-3\
-  --min_lr 1e-4\
-  --rlrp_factor 0.2\
-  --rlrp_patience 5\
-  --weight_decay 5e-6\
-  --rotation_range 30\
-  --sharpen_p 0.2\
-  --contrast_gamma 30\
-  --clae_p 0.5 \
-  --n_gender_dcs 0 \
-  --sex_sigma 1\
-  --age_sigma 1
+    --name $NAME \
+    --config=configs/xcat.yml \
+    --data.img_dir="$DATA_DIR/annotated" \
+    --data.mask_dirs=["$DATA_DIR/masks/tensormask","$DATA_DIR/masks/unet"] \
+    --config=configs/default_model.yml \
+    --model.dense_layers=[256]
--- bash/train_model.sh
+++ bash/train_model.sh
@@ -1,24 +1,17 @@
-NAME="effnet_default"
-LOG_FILE=output/$NAME/"version_0.log"
-mkdir output/$NAME
+#! /bin/bash
+NAME="model_name"
 
-PARAMS=(
-    --name $NAME
-    --gpus 1
-    --precision 16
-    --cache_data
-    --max_time 00:09:00:00
-    --input_height 512
-    --input_width 512
-    --num_workers 16
-    --max_epoch 100
-    --batch_size 128
-    --model dbam_efficientnet-b0
-    --mask_dir "../TensorMask/output/Colab_train/inference/bone_age" "../TensorMask/output/Colab_train/inference_alexander/bone_age_train"
-    --cache_data
-    --lr 1e-3
-    --min_lr 1e-4
-    --rlrp_factor 0.2
-)
+DATA_DIR="../data"
 
-LOG_FILE=$LOG_FILE python train_model.py ${PARAMS[@]}
+out_root=output/$NAME
+mkdir output
+mkdir "$out_root"
+LOG_FILE="$out_root/version_0".log
+
+LOG_FILE=$LOG_FILE python train_model.py \
+    --name $NAME \
+    --config=configs/xcat.yml \
+    --data.img_dir="$DATA_DIR/annotated" \
+    --data.mask_dirs=["$DATA_DIR/masks/tensormask", "$DATA_DIR/masks/unet"] \
+    --config=configs/default_model.yml \
+    --model.dense_layers=[256]
